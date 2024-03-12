318dd8bba2b81f8275c88636eb48474937af2061
Tasnim Hoque Sristy <hoque.tasnim05@gmail.com>
2024-03-12 18:06:39 +0600

    backend

--- imsFrontend/src/Router/router.js
+++ imsFrontend/src/Router/router.js
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import TestPage from "../TestPage";
+import Login from "../Pages/Login/Login";
 
 const router = createBrowserRouter([
     {
         path: '/login',
-        element: <TestPage/>,
+        element: <Login/>,
     },
     {
         path: '/',
---
+++ imsFrontend/src/config.js
@@ -0,0 +1,1 @@
+export const URL = "http://10.100.18.206:4321"
