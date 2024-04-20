28884e4226100f2dcb9b1084f188d0322204a7b2
arifulislam08173 <5seariful15@gmail.com>
2024-04-20 18:41:44 +0600

    Dashboard design

--- imsFrontend/src/Pages/Home/Dashboard.jsx
+++ imsFrontend/src/Pages/Home/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link, Outlet } from 'react-router-dom';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
@@ -7,16 +7,16 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const DashboardSidebar = () => {
 
   return (
-      <div className='col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark'>
-          <div className='d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100'>
+      <div className='col-auto col-md-3 col-xl-2 px-sm-2 px-0' style={{ backgroundColor: 'white'}}>
+          <div className='d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-dark min-vh-100'>
 
-              <Link 
-                  className='d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none'
+              {/* <Link 
+                  className='d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-dark text-decoration-none'
                   to="/home">
                   <span className='fs-5 fw-bolder d-none d-sm-inline'>
                       Admin Dashboard
                   </span>
-              </Link>
+              </Link> */}
 
               <ul 
                   className='nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start'
@@ -24,56 +24,67 @@ const DashboardSidebar = () => {
 
                   <li className='w-100'>
                       <Link 
-                          className='nav-link text-white px-0 align-middle'
-                          to="/home">
+                          className='nav-link text-dark px-0 align-middle'
+                          to="/home" >
                           <i className='fs-4 bi-speedometer2 ms-2'></i>
                           <span className='ms-2 d-none d-sm-inline'>Dashboard</span>
                       </Link>
+                      <hr />
                   </li>
 
                   <li className='w-100'>
                       <Link 
-                          className='nav-link text-white px-0 align-middle'
+                          className='nav-link text-dark px-0 align-middle'
                           to="/category">
                           <i className='fs-4 bi-columns ms-2'></i>
                           <span className='ms-2 d-none d-sm-inline'>Category</span>
                       </Link>
+                      <hr />
+
                   </li>
 
                   <li className='w-100'>
                       <Link 
-                          className='nav-link text-white px-0 align-middle'
+                          className='nav-link text-dark px-0 align-middle'
                           to="/product">
                           <i className='fs-4 bi-shop ms-2'></i>
                           <span className='ms-2 d-none d-sm-inline'>Product</span>
                       </Link>
+                      <hr />
+
                   </li>
 
                   <li className='w-100'>
                       <Link 
-                          className='nav-link text-white px-0 align-middle'
+                          className='nav-link text-dark px-0 align-middle'
                           to="/transactionMaster">
                           <i className='fs-4 bi-cash ms-2'></i>
                           <span className='ms-2 d-none d-sm-inline'>Transaction Master</span>
                       </Link>
+                      <hr />
+
                   </li>
 
                   <li className='w-100'>
                       <Link 
-                          className='nav-link text-white px-0 align-middle'
+                          className='nav-link text-dark px-0 align-middle'
                           to="/transactionDetails">
                           <i className='fs-4 bi-ticket-detailed-fill ms-2'></i>
                           <span className='ms-2 d-none d-sm-inline'>Transaction Details</span>
                       </Link>
+                      <hr />
+
                   </li>
 
                   <li className='w-100'>
                       <Link 
-                          className='nav-link text-white px-0 align-middle'
+                          className='nav-link text-dark px-0 align-middle'
                           to="/home/profile">
                           <i className='fs-4 bi-person ms-2'></i>
                           <span className='ms-2 d-none d-sm-inline'>Profile</span>
                       </Link>
+                      <hr />
+
                   </li>
               </ul>
           </div>
@@ -83,32 +94,24 @@ const DashboardSidebar = () => {
 
 // DashboardTopBar
 const DashboardTopBar = () => {
-  const [dropDown, setDropdown] = useState(false);
-
-  const toggleDropdown = () => {
-      setDropdown(!dropDown);
-  };
+    const handleLogout = () => {
+        // handle logout functionality
+    };
 
   return (
-      <div className='p-2 d-flex justify-content-between shadow'>
-          <h4>Inventory Management</h4>
+      <div className='p-2 d-flex justify-content-between align-items-center text-white w-100' style={{backgroundColor: '#53A0F1'}}>
+          {/* <h4></h4> */}
+          <div></div>
 
-          <div className="dropdown ml-auto" style={{ marginRight: '50px' }}>
+          <div className="mr-3">
               <button 
-                  className="btn btn-secondary dropdown-toggle" 
+                  className="btn btn-secondary" 
                   type="button" 
-                  id="dropdownMenuButton" 
-                  onClick={toggleDropdown}
-                  aria-expanded={dropDown ? "true" : "false"}
+                  id="logoutButton" 
+                  onClick={handleLogout}
               >
-                  <i className="bi bi-person-circle fs-4"></i>
+                  <i className="bi bi-power fs-4"></i>
               </button>
-              <ul className={`dropdown-menu ${dropDown ? 'show' : ''} dropdown-menu-right`} aria-labelledby="dropdownMenuButton">
-                  <li><Link className='dropdown-item' to="/dashboard/logout">
-                      <i className='fs-4 bi-power ms-2'></i>
-                      Logout
-                  </Link></li>
-              </ul>
           </div>
       </div>
   );
@@ -118,11 +121,11 @@ const Dashboard = () => {
 
 
   return (
-    <div className='container-fluid'>
-      <div className='row flex-nowrap'>
+    <div className='container-fluid p-0'>
+        <DashboardTopBar /> 
+        <div className='row flex-nowrap'>
                 <DashboardSidebar /> 
                 <div className='col p-0 m-0'>
-                    <DashboardTopBar /> 
                     <div className='p-2'>
                         <Outlet />
                     </div>
