c49049add9d17aa07e56185d3a7d008eb2c25da0
Sabrina Haque <sabrina.haque442@gmail.com>
2024-04-28 14:35:24 +0600

    t

--- imsFrontend/src/Pages/404/NoMatch.jsx
+++ imsFrontend/src/Pages/404/NoMatch.jsx
@@ -6,6 +6,7 @@ const NoMatch = () => {
             THIS IS FOR NO MATCHING PAGES
             <div>
                 <h1>Testing</h1>
+                <h2>ttsing from sabrina pc</h2>
             </div>
         </>
     )
